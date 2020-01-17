5bad00d4046efdc24d029bb90b65594c51b8ff33
zhangzhengyan <zzy_19831106@163.com>
2020-01-17 14:36:03 +0800

    对中继和通过中继的连接信息做记录 对连接被引用的次数做记录

--- clientStore/connManager.go
+++ clientStore/connManager.go
@@ -3,16 +3,18 @@ package clientStore
 import (
 	"context"
 	"fmt"
-	ci "github.com/yottachain/YTHost/clientInterface"
 	"github.com/libp2p/go-libp2p-core/peer"
-	"github.com/mr-tron/base58"
 	"github.com/multiformats/go-multiaddr"
+	ci "github.com/yottachain/YTHost/clientInterface"
+	"github.com/yottachain/YTHost/util"
 	"sync"
 )
 
 type ClientStore struct {
 	connect func(ctx context.Context, id peer.ID, mas []multiaddr.Multiaddr) (ci.YTHClient, error)
 	sync.Map
+	l sync.Mutex	//对下面map进行加锁
+	connTopid map[ci.YTHClient] []peer.ID //记录一个链接被多少个pid使用，如果通过中继建立的链接那么一个实际链接对应多个pid
 }
 
 // Get 获取一个客户端，如果没有，建立新的客户端连接
@@ -40,10 +42,27 @@ start:
 	_c, ok := cs.Map.Load(pid)
 	// 如果不存在创建新的clt
 	if !ok {
+		relayID := util.GetRealyId(mas)
+		if relayID.String() != "" {
+			cli, ok := cs.Map.Load(relayID)
+
+			if ok {
+				c := cli.(ci.YTHClient)
+				cs.Map.Store(pid, c)
+				cs.storeConnInfo(pid, c)
+				return c, nil
+			}
+		}
+
 		if clt, err := cs.connect(ctx, pid, mas); err != nil {
 			return nil, err
 		} else {
 			cs.Map.Store(pid, clt)
+			cs.storeConnInfo(pid, clt)
+			if relayID.String() != "" {
+				cs.Map.Store(relayID, clt)
+				cs.storeConnInfo(pid, clt)
+			}
 			// 创建clt完成后返回到开始
 			goto start
 		}
@@ -60,8 +79,9 @@ start:
 }
 
 func (cs *ClientStore) GetByAddrString(ctx context.Context, id string, addrs []string) (ci.YTHClient, error) {
-	buf, _ := base58.Decode(id)
-	pid, err := peer.IDFromBytes(buf)
+	/*buf, _ := base58.Decode(id)
+	pid, err := peer.IDFromBytes(buf)*/
+	pid, err := peer.Decode(id)
 	if err != nil {
 		return nil, err
 	}
@@ -87,7 +107,9 @@ func (cs *ClientStore) Close(pid peer.ID) error {
 	clt := _clt.(ci.YTHClient)
 
 	cs.Map.Delete(pid)
-	return clt.Close()
+
+	return cs.delConnInfo(pid, clt)
+	//return clt.Close()
 }
 
 func (cs *ClientStore) GetClient(pid peer.ID) (ci.YTHClient, bool) {
@@ -100,6 +122,57 @@ func (cs *ClientStore) GetClient(pid peer.ID) (ci.YTHClient, bool) {
 	return nil, ok
 }
 
+func (cs *ClientStore) storeConnInfo(pid peer.ID, clt ci.YTHClient) () {
+	cs.l.Lock()
+	defer cs.l.Unlock()
+	pids, ok := cs.connTopid[clt]
+
+	if ok {
+		cs.connTopid[clt] = append(pids, pid)
+	}else {
+		pids = make([]peer.ID, 1)
+		pids[0] = pid
+		cs.connTopid[clt] = pids
+	}
+}
+
+func (cs *ClientStore) delConnInfo(pid peer.ID, clt ci.YTHClient) error {
+	cs.l.Lock()
+	defer cs.l.Unlock()
+	pids, ok := cs.connTopid[clt]
+
+	if ok {
+		for i, c_pid := range pids {
+			if pid == c_pid {
+				pids = append(pids[:i], pids[i+1:]...)
+				cs.connTopid[clt] = pids
+				break
+			}
+		}
+
+		if len(pids) == 0 {
+			delete(cs.connTopid, clt)
+			return clt.Close()
+		}else {
+			return nil
+		}
+	}else {
+		return fmt.Errorf("peer id matching clt error!")
+	}
+}
+
+func (cs *ClientStore) PrintConnInfo(clt ci.YTHClient) {
+	cs.l.Lock()
+	defer cs.l.Unlock()
+	pids, ok := cs.connTopid[clt]
+	if ok {
+		lenth := len(pids)
+		for i := 0; i < lenth; i++ {
+			fmt.Println(pids[i].String())
+		}
+	}
+}
+
 // Len 返回当前连接数
 //func (cs *ClientStore) Len() int {
 //}
@@ -108,5 +181,7 @@ func NewClientStore(connFunc func(ctx context.Context, id peer.ID, mas []multiad
 	return &ClientStore{
 		connFunc,
 		sync.Map{},
+		sync.Mutex{},
+		map[ci.YTHClient][]peer.ID{},
 	}
 }
